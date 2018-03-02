ffc5362a362e74071f516c6ecb4a6348923a7999
zhangchao <zhangchao@floruit.cn>
2018-03-02 11:25:15 +0800

    package

--- app/src/main/java/base/zc/com/project/MainActivity.java
+++ app/src/main/java/base/zc/com/project/MainActivity.java
@@ -1,13 +1,21 @@
-package base.zc.com.zcbaseproject;
+package base.zc.com.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_main = findViewById(R.id.iv_main);
+        Glide.with(this).load("https://img.cuixianyuan.com/template/home/05723576862242212.jpg").into(iv_main);
     }
 }
