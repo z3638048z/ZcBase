1eaad5d07f5d65c01a4576826349136dad592f4f
zhangchao <zhangchao@floruit.cn>
2018-03-02 09:39:40 +0800

    glide

---
+++ zc-base/src/main/java/com/zc/base/glideutil/UnsafeOkHttpGlideModule.java
@@ -0,0 +1,27 @@
+package com.zc.base.glideutil;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.module.GlideModule;
+
+import java.io.InputStream;
+
+/**
+ * Created by Darren on 2016/11/29.
+ */
+
+public class UnsafeOkHttpGlideModule implements GlideModule {
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+        glide.register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory());
+    }
+}
